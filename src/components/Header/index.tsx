8ca9b06e925cf0c5667dbe6e5afb1431ffc5e6af
Joubert Natividade <joubert.devba@gmail.com>
2022-02-07 11:55:21 -0300

    Inicio do app

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles";
+
+
+
+export function Header() {
+    return (
+        <Container>
+            <button>Novo cadastro</button>
+        </Container>
+    )
+}
